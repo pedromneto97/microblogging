225eddff16bd490cb88a95a7218d1db93819892b
Pedro Neto <pedromneto97@gmail.com>
2021-05-20 20:36:48 -0300

    feat: Tema do aplicativo

---
+++ lib/design_system/theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+const _colorScheme = ColorScheme.light();
+
+final theme = ThemeData(
+  brightness: _colorScheme.brightness,
+  primaryColor: _colorScheme.primary,
+  primaryColorBrightness:
+      ThemeData.estimateBrightnessForColor(_colorScheme.primary),
+  canvasColor: _colorScheme.background,
+  accentColor: _colorScheme.secondary,
+  accentColorBrightness:
+      ThemeData.estimateBrightnessForColor(_colorScheme.secondary),
+  scaffoldBackgroundColor: _colorScheme.background,
+  bottomAppBarColor: _colorScheme.surface,
+  cardColor: _colorScheme.surface,
+  dividerColor: _colorScheme.onSurface.withOpacity(0.12),
+  backgroundColor: _colorScheme.background,
+  dialogBackgroundColor: _colorScheme.background,
+  errorColor: _colorScheme.error,
+  indicatorColor: _colorScheme.onPrimary,
+  applyElevationOverlayColor: false,
+  colorScheme: _colorScheme,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStateProperty.all<OutlinedBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+      ),
+      minimumSize: MaterialStateProperty.all<Size>(const Size.fromHeight(48)),
+    ),
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: ButtonStyle(
+      shape: MaterialStateProperty.all<OutlinedBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+      ),
+      minimumSize: MaterialStateProperty.all<Size>(const Size.fromHeight(48)),
+    ),
+  ),
+);
