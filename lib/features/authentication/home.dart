18824b7df3c71167e0e0980680d0cf3d43c6097d
Pedro Neto <pedromneto97@gmail.com>
2021-05-20 19:26:30 -0300

    feat: Ajusta primeira tela do app

--- lib/features/authentication/home.dart
+++ lib/features/authentication/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  static const screenName = 'home_screen';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
