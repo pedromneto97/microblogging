22c6edc5809dbe97f27eafb31c924856afa1e235
Pedro Neto <pedromneto97@gmail.com>
2021-05-24 22:14:49 -0300

    feat: Repositório de autenticação

---
+++ lib/repository/register_repository.dart
@@ -0,0 +1,44 @@
+import 'package:hive/hive.dart';
+
+import '../box/user_box.dart';
+import '../models/exceptions.dart';
+import '../models/user.dart';
+
+class AuthenticationRepository {
+  final Box<UserBox> box;
+
+  const AuthenticationRepository._({
+    required this.box,
+  });
+
+  static AuthenticationRepository? _instance;
+
+  factory AuthenticationRepository({required Box<UserBox> box}) {
+    _instance ??= AuthenticationRepository._(box: box);
+    return _instance as AuthenticationRepository;
+  }
+
+  User registerUser({
+    required String email,
+    required String password,
+    required String name,
+  }) {
+    final filteredUsers = box.values
+        .where((user) => user.email == email && user.password == password);
+    if (filteredUsers.isNotEmpty) {
+      throw const UserAlreadyExists(message: 'Usuário já cadastrado');
+    }
+    box.add(
+      UserBox()
+        ..name = name
+        ..password = password
+        ..email = email,
+    );
+
+    return User(
+      name: name,
+      email: email,
+      password: password,
+    );
+  }
+}
