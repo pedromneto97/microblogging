e3242425cc8fb9cd879c786097c4d421a5c19caf
Pedro Neto <pedromneto97@gmail.com>
2021-05-24 22:16:15 -0300

    feat: Ajusta blocs e hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import 'blocs/authentication/authentication/authentication_bloc.dart';
+import 'box/user_box.dart';
 import 'design_system/theme.dart';
 import 'features/authentication/login/login.dart';
 import 'features/authentication/register/register.dart';
+import 'features/feed/feed/feed.dart';
+import 'repository/register_repository.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserBoxAdapter());
+  await Hive.openBox<UserBox>('users');
   runApp(MyApp());
 }
 
@@ -12,14 +22,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Seu blog',
-      theme: theme,
-      initialRoute: Home.screenName,
-      routes: {
-        Home.screenName: (context) => const Home(),
-        Register.screenName: (context) => const Register(),
-      },
+    return RepositoryProvider(
+      create: (context) => AuthenticationRepository(
+        box: Hive.box<UserBox>('users'),
+      ),
+      lazy: false,
+      child: BlocProvider(
+        create: (context) => AuthenticationBloc(
+          authenticationRepository:
+              RepositoryProvider.of<AuthenticationRepository>(context),
+        ),
+        lazy: false,
+        child: MaterialApp(
+          title: 'Seu blog',
+          theme: theme,
+          initialRoute: Login.screenName,
+          routes: {
+            Login.screenName: (context) => const Login(),
+            Register.screenName: (context) => const Register(),
+            Feed.screenName: (context) => const Feed(),
+          },
+        ),
+      ),
     );
   }
 }
