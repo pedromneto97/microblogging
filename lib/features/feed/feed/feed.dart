4e43ed6b85b8f785844e57dda2c7dcc26d2f18a4
Pedro Neto <pedromneto97@gmail.com>
2021-05-24 22:16:05 -0300

    feat: Inicio do feed

---
+++ lib/features/feed/feed/feed.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Feed extends StatelessWidget {
+  static const String screenName = 'feed_sceen';
+
+  const Feed();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Feed'),
+      ),
+    );
+  }
+}
