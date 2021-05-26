b950143c83e8bf47f504762a404746ca8ff6a749
Pedro Neto <pedromneto97@gmail.com>
2021-05-25 22:38:05 -0300

    feat: Adiciona bloc a tela

--- lib/features/feed/bottom_tab_container.dart
+++ lib/features/feed/bottom_tab_container.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../blocs/authentication/authentication/authentication_bloc.dart';
+import '../../repository/post_repository.dart';
+import 'bloc/feed/feed_bloc.dart';
 import 'feed/feed.dart';
 
 class BottomTabContainer extends StatefulWidget {
@@ -26,41 +28,51 @@ class _BottomTabContainerState extends State<BottomTabContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-          builder: (context, state) => Text(
-            'Bem vindo ${(state as AuthenticationSuccessState).user.name}!',
+    return RepositoryProvider(
+      create: (context) => PostRepository(),
+      lazy: false,
+      child: BlocProvider(
+        create: (context) => FeedBloc(
+          postRepository: RepositoryProvider.of<PostRepository>(context),
+        ),
+        lazy: false,
+        child: Scaffold(
+          appBar: AppBar(
+            title: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+              builder: (context, state) => Text(
+                'Bem vindo ${(state as AuthenticationSuccessState).user.name}!',
+              ),
+            ),
+          ),
+          body: const Feed(),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: index,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.business),
+                label: 'Business',
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.school),
+                label: 'School',
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.settings),
+                label: 'Settings',
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+            ],
+            onTap: onPressBottomTab,
+            iconSize: 24,
           ),
         ),
-      ),
-      body: const Feed(),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: index,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Theme.of(context).primaryColor,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.business),
-            label: 'Business',
-            backgroundColor: Theme.of(context).primaryColor,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.school),
-            label: 'School',
-            backgroundColor: Theme.of(context).primaryColor,
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.settings),
-            label: 'Settings',
-            backgroundColor: Theme.of(context).primaryColor,
-          ),
-        ],
-        onTap: onPressBottomTab,
-        iconSize: 24,
       ),
     );
   }
