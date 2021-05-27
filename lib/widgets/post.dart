0639f1862d78168124dea93ca2325f51246678af
Pedro Neto <pedromneto97@gmail.com>
2021-05-26 22:39:21 -0300

    feat: ajusta post para ter uma imagem ao lado

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -20,32 +20,55 @@ class PostCard extends StatelessWidget {
       margin: const EdgeInsets.all(8),
       child: Container(
         padding: const EdgeInsets.all(16.0),
-        height: 160,
-        child: Column(
+        height: 176,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            if (post.user.photoUrl != null)
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Image.network(
+                  post.user.photoUrl!,
+                  errorBuilder: (context, _, __) {
+                    return CircleAvatar(
+                      child: Container(
+                        child: Text(post.user.name[0]),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Text(
-                    post.user.name,
-                    style: Theme.of(context).textTheme.headline6,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          post.user.name,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                          DateTime.now().difference(post.dateTime).inDays > 0
+                              ? _formatDateTime.format(post.dateTime)
+                              : _formatTime.format(post.dateTime),
+                          style: Theme.of(context).textTheme.caption,
+                        )
+                      ],
+                    ),
                   ),
                   Text(
-                    DateTime.now().difference(post.dateTime).inDays > 0
-                        ? _formatDateTime.format(post.dateTime)
-                        : _formatTime.format(post.dateTime),
-                    style: Theme.of(context).textTheme.caption,
+                    post.text,
+                    style: Theme.of(context).textTheme.bodyText2,
                   )
                 ],
               ),
             ),
-            Text(
-              post.text,
-              style: Theme.of(context).textTheme.bodyText2,
-            )
           ],
         ),
       ),
