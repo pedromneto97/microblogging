72ef296ca05720a54cefd6b037d945352427593f
Pedro Neto <pedromneto97@gmail.com>
2021-05-28 21:27:28 -0300

    feat: on tap do card

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -9,70 +9,75 @@ final DateFormat _formatTime = DateFormat('HH:mm');
 class PostCard extends StatelessWidget {
   final Post post;
   final bool useFirstName;
+  final void Function()? onTap;
 
   const PostCard({
     Key? key,
     required this.post,
     this.useFirstName = true,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(8),
-      child: Container(
-        padding: const EdgeInsets.all(16.0),
-        height: 176,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            if (post.user.photoUrl != null)
-              Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Image.network(
-                  post.user.photoUrl!,
-                  errorBuilder: (context, _, __) {
-                    return CircleAvatar(
-                      child: Container(
-                        child: Text(post.user.name[0]),
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          height: 176,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              if (post.user.photoUrl != null)
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Image.network(
+                    post.user.photoUrl!,
+                    errorBuilder: (context, _, __) {
+                      return CircleAvatar(
+                        child: Container(
+                          child: Text(post.user.name[0]),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            useFirstName ? post.user.firstName : post.user.name,
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          Text(
+                            DateTime.now().difference(post.dateTime).inDays > 0
+                                ? _formatDateTime.format(post.dateTime)
+                                : _formatTime.format(post.dateTime),
+                            style: Theme.of(context).textTheme.caption,
+                          )
+                        ],
                       ),
-                    );
-                  },
+                    ),
+                    Text(
+                      post.text,
+                      style: Theme.of(context).textTheme.bodyText2,
+                    )
+                  ],
                 ),
               ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          useFirstName ? post.user.firstName : post.user.name,
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                        Text(
-                          DateTime.now().difference(post.dateTime).inDays > 0
-                              ? _formatDateTime.format(post.dateTime)
-                              : _formatTime.format(post.dateTime),
-                          style: Theme.of(context).textTheme.caption,
-                        )
-                      ],
-                    ),
-                  ),
-                  Text(
-                    post.text,
-                    style: Theme.of(context).textTheme.bodyText2,
-                  )
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
