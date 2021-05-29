8f14039cf3b9d34a5c4eb704226ac5a075f3ec9c
Pedro Neto <pedromneto97@gmail.com>
2021-05-29 09:56:00 -0300

    feat: Usa widget base

--- lib/features/menu/widgets/logout_modal.dart
+++ lib/features/menu/widgets/logout_modal.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../blocs/authentication/authentication/authentication_bloc.dart';
+import '../../../widgets/my_dialog.dart';
 import '../../authentication/login/login.dart';
 
 class LogoutModal extends StatelessWidget {
@@ -9,60 +10,53 @@ class LogoutModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Deseja realmente sair?',
-              style: Theme.of(context).textTheme.headline6,
-              textAlign: TextAlign.center,
+    return MyDialog(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Deseja realmente sair?',
+            style: Theme.of(context).textTheme.headline6,
+            textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 32.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('NÃO'),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await Navigator.of(context).pushNamedAndRemoveUntil(
+                          Login.screenName,
+                          (route) => route.settings.name == '/',
+                        );
+                        BlocProvider.of<AuthenticationBloc>(context).add(
+                          const LogoutEvent(),
+                        );
+                      },
+                      child: const Text('SIM'),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 32.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Flexible(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: OutlinedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('NÃO'),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          await Navigator.of(context).pushNamedAndRemoveUntil(
-                            Login.screenName,
-                            (route) => route.settings.name == '/',
-                          );
-                          BlocProvider.of<AuthenticationBloc>(context).add(
-                            const LogoutEvent(),
-                          );
-                        },
-                        child: const Text('SIM'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
