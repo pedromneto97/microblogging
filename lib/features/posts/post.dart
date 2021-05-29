e69f5604229a00851089ba33150e3ba2878015a2
Pedro Neto <pedromneto97@gmail.com>
2021-05-28 22:28:06 -0300

    feat: ajusta para navegar para a tela atualizada

--- lib/features/posts/post.dart
+++ lib/features/posts/post.dart
@@ -4,7 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../blocs/authentication/authentication/authentication_bloc.dart';
 import '../../models/post.dart';
 import '../../repository/post_repository.dart';
+import '../bottom_tab/bottom_tab_container.dart';
 import 'bloc/post_crud/post_crud_bloc.dart';
+import 'my_posts.dart';
 
 class PostScreen extends StatefulWidget {
   static const screenName = 'post_screen';
@@ -49,96 +51,103 @@ class _PostScreenState extends State<PostScreen> {
           title:
               Text(widget.post == null ? 'Criar novo post' : 'Editando post'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 24.0),
-                      child: Text(
-                        'O que você está pensando?'.toUpperCase(),
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ),
-                    TextFormField(
-                      controller: _textEditingController,
-                      decoration: const InputDecoration(
-                        labelText: 'Texto',
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                      ),
-                      maxLines: 7,
-                      maxLength: 280,
-                      validator: (value) {
-                        if (value == null || value.length < 3) {
-                          return 'O texto é obrigatório';
-                        }
-                        return null;
-                      },
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    if (widget.post != null)
-                      Builder(
-                        builder: (context) => Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: OutlinedButton(
-                            onPressed: () =>
-                                BlocProvider.of<PostCrudBloc>(context).add(
-                              PostCrudEventRemove(id: widget.post!.id),
-                            ),
-                            child: const Text('APAGAR'),
-                          ),
+        body: BlocListener<PostCrudBloc, PostCrudState>(
+          listener: (context, state) {
+            if (state is SuccessPostCrudState) {
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                MyPosts.screenName,
+                ModalRoute.withName(BottomTabContainer.screenName),
+              );
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 24.0),
+                        child: Text(
+                          'O que você está pensando?'.toUpperCase(),
+                          style: Theme.of(context).textTheme.bodyText1,
                         ),
                       ),
-                    BlocBuilder<PostCrudBloc, PostCrudState>(
-                      builder: (context, state) {
-                        if (state is SuccessPostCrudState) {
-                          Navigator.of(context).pop();
-                        }
-                        if (state is InProgressPostCrudState) {
-                          return const ElevatedButton(
-                            onPressed: null,
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                        return ElevatedButton(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              BlocProvider.of<PostCrudBloc>(context).add(
-                                widget.post == null
-                                    ? PostCrudEventCreate(
-                                        text: _textEditingController.text,
-                                        userId: (BlocProvider.of<
-                                                AuthenticationBloc>(
-                                          context,
-                                        ).state as AuthenticationSuccessState)
-                                            .user
-                                            .id,
-                                      )
-                                    : PostCrudEventEdit(
-                                        id: widget.post!.id,
-                                        text: _textEditingController.text,
-                                      ),
-                              );
-                            }
-                          },
-                          child: Text(
-                            widget.post == null ? 'PUBLICAR' : 'EDITAR',
+                      TextFormField(
+                        controller: _textEditingController,
+                        decoration: const InputDecoration(
+                          labelText: 'Texto',
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                        ),
+                        maxLines: 7,
+                        maxLength: 280,
+                        validator: (value) {
+                          if (value == null || value.length < 3) {
+                            return 'O texto é obrigatório';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      if (widget.post != null)
+                        Builder(
+                          builder: (context) => Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: OutlinedButton(
+                              onPressed: () =>
+                                  BlocProvider.of<PostCrudBloc>(context).add(
+                                PostCrudEventRemove(id: widget.post!.id),
+                              ),
+                              child: const Text('APAGAR'),
+                            ),
                           ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ],
+                        ),
+                      BlocBuilder<PostCrudBloc, PostCrudState>(
+                        builder: (context, state) {
+                          if (state is InProgressPostCrudState) {
+                            return const ElevatedButton(
+                              onPressed: null,
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          return ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                BlocProvider.of<PostCrudBloc>(context).add(
+                                  widget.post == null
+                                      ? PostCrudEventCreate(
+                                          text: _textEditingController.text,
+                                          userId: (BlocProvider.of<
+                                                  AuthenticationBloc>(
+                                            context,
+                                          ).state as AuthenticationSuccessState)
+                                              .user
+                                              .id,
+                                        )
+                                      : PostCrudEventEdit(
+                                          id: widget.post!.id,
+                                          text: _textEditingController.text,
+                                        ),
+                                );
+                              }
+                            },
+                            child: Text(
+                              widget.post == null ? 'PUBLICAR' : 'EDITAR',
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
